974a7511c95977b2c8950e923795c248169adec3
Valeriy Klimchenko <klimmail.ru@mail.ru>
2023-04-15 17:44:28 +0300

    add override properties v2

--- Inheritance.playground/Contents.swift
+++ Inheritance.playground/Contents.swift
@@ -3,14 +3,14 @@ enum Country {
 }
 
 enum Genre {
-    case rock, opera, pop
+    case rock, opera, pop, errorGenre
 }
 
 class Artist {
     
-    let name: String
-    let country: Country
-    let genre: Genre
+    var name: String
+    var country: Country
+    var genre: Genre
     
     init(name: String, country: Country, genre: Genre) {
         self.name = name
@@ -19,26 +19,59 @@ class Artist {
     }
     
     func whriteTrack(_ trackName: String) {
-        print("\(self.name) just write \("trackName")")
+        print("\(self.name) write music")
     }
     
     func playTrack(_ trackName: String) {
-        print("\(self.name) playing \("trackName")")
+        print("\(self.name) playing music")
     }
 }
+
+//var artist = Artist(name: "Artist", country: Country.GB, genre: Genre.opera)
+//artist.whriteTrack("New Track")
+//artist.playTrack("New Track")
+
+
 
 final class RockArtist: Artist {
-
-    override func whriteTrack(_ trackName: String) {
+    
+    override var genre: Genre {
+        get { Genre.rock }
+        set { print(Genre.rock) }
+    }
+    
+    var longHair: Bool
+    
+    override init(name: String, country: Country, genre: Genre) {
+        self.longHair = true
+        super.init(name: name, country: country, genre: genre)
+    }
+    
+    override func playTrack(_ trackName: String) {
         print("Rock-artist \(self.name) play rock-n-roll")
     }
+    
+    override func whriteTrack(_ trackName: String) {
+        print("\(self.name) just write \(trackName)")
+    }
 }
+
+
+var rockArtist = RockArtist(name: "", country: Country.GB, genre: Genre.errorGenre)
+rockArtist.playTrack("New Track")
+rockArtist.whriteTrack("New Track")
+rockArtist.genre
 
 
 
 final class StreetArtist: Artist {
     
     let isUnderRoof: Bool
+    
+    override var genre: Genre {
+        get { Genre.pop }
+        set { print(Genre.pop) }
+    }
     
     init(isUnderRoof: Bool, name: String, country: Country, genre: Genre) {
         self.isUnderRoof = isUnderRoof
@@ -53,26 +86,48 @@ final class StreetArtist: Artist {
         }
     }
     
+    override func whriteTrack(_ trackName: String) {
+        print("\(self.name) can`t to write music")
+    }
 }
+
+var popArtist = StreetArtist(isUnderRoof: true, name: "Pop Artist", country: Country.usa, genre: Genre.errorGenre)
+popArtist.genre
+
 
 
 final class TheaterArtist: Artist {
     
     let isBoring: Bool
     
+    override var genre: Genre {
+        get { Genre.opera }
+        set { Genre.opera }
+    }
+    
     override init(name: String, country: Country, genre: Genre) {
         self.isBoring = true
         super.init(name: name, country: country, genre: genre)
     }
     
+    override func whriteTrack(_ sonataName: String) {
+        print("\(self.name) write a greate music \(sonataName)")
+    }
+    
     override func playTrack(_ songName: String) {
         if isBoring {
-            print("the audience falls asleep")
+            print("the audience falls a sleep")
         } else {
             print("\(self.name) playing \(songName)")
         }
     }
 }
+
+var operaArtist = TheaterArtist(name: "Opera Artist", country: Country.russia, genre: Genre.pop)
+operaArtist.genre = Genre.errorGenre
+operaArtist.genre
+
+
 var array = [Artist]()
 let tarjaTurunen = RockArtist(name: "tarjaTurunen", country: Country.finland, genre: Genre.rock)
 let operaSinger = TheaterArtist(name: "operaSonger", country: Country.russia, genre: Genre.opera)
@@ -81,4 +136,10 @@ array.append(tarjaTurunen)
 array.append(operaSinger)
 array.append(streetSonger)
 array
+
+tarjaTurunen.whriteTrack("123")
+
+
+
+
 //Тут все просто. В задании с треками разобрался с созданием классов, их наследованием, переопределением методов и свойств, инициализацией (когда надо, когда не надо, как использовать супер инит). Повторил что такое словари и массивы, вспомнил как получать доступ к элементам и добавлять/удалять их. Применил перечисления на практике
