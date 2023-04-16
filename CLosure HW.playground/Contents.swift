e78b88bea671f9ff11d74f30374ad7b4bb3f9984
Valeriy Klimchenko <klimmail.ru@mail.ru>
2023-04-16 03:28:15 +0300

    .

---
+++ CLosure HW.playground/Contents.swift
@@ -0,0 +1,34 @@
+//Task1
+let satelliteWeight: Double = 333
+let flightLenght = 999.999
+
+func calculateFuelConsumption(satelliteWeight: Double, flightLenght: Double, algoritm: (Double, Double) -> Double) -> Double {
+    return algoritm(satelliteWeight, flightLenght)
+}
+
+let firstAlgoritm = { (m: Double, d: Double) -> Double in return m * d * 1.340716 }
+let secondAlgoritm = { (m: Double, d: Double) -> Double in return m * d * 1.234716 }
+
+calculateFuelConsumption(satelliteWeight: satelliteWeight, flightLenght: flightLenght, algoritm: firstAlgoritm)
+calculateFuelConsumption(satelliteWeight: satelliteWeight, flightLenght: flightLenght, algoritm: secondAlgoritm)
+
+//Task2
+
+let firstShortAlgoritm = { $0 * $1 * 1.340716}
+let secondShortAlgoritm = { $0 * $1 * 1.234716}
+calculateFuelConsumption(satelliteWeight: satelliteWeight, flightLenght: flightLenght, algoritm: firstAlgoritm)
+calculateFuelConsumption(satelliteWeight: satelliteWeight, flightLenght: flightLenght, algoritm: secondAlgoritm)
+
+
+//Task3
+
+let myAlgoritm = calculateFuelConsumption(satelliteWeight: 333, flightLenght: 999.999) { $0 * $1 * 1.48573829 / 2 }
+
+//calculateFuelConsumption() { _,_ in 333 * 999 }
+
+print(myAlgoritm)
+
+
+
+
+
